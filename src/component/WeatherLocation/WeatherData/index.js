bbe38df9bea131e5f34d0191e48d13e54b7fa089
Lala <oscarito_16m@live.com.mx>
2019-04-05 22:44:37 -0600

    Seccion 6 #38

--- src/component/WeatherLocation/WeatherData/index.js
+++ src/component/WeatherLocation/WeatherData/index.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import {SUN} from '../../../constants/weathers';
 import './styles.css';
 
 class WeatherData extends Component{
     render(){
         return(
             <div className="weatherDataCont">
-                <WeatherTemperature temperature={20} weatherState={SUN}/>
-                <WeatherExtraInfo humidity={80} win={'10 m/s'}/>
+                <WeatherTemperature temperature={this.props.data.temperature} 
+                weatherState={this.props.data.weatherState}/>
+                <WeatherExtraInfo humidity={this.props.data.humidity} 
+                win={this.props.data.wind}/>
             </div>
         )
     }
+}
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
 }
 
 export default WeatherData;
--- src/component/WeatherLocation/index.js
+++ src/component/WeatherLocation/index.js
@@ -1,14 +1,22 @@
 import React, { Component }from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData/index';
+import {SUN} from '../../constants/weathers';
 import './styles.css';
+
+const data = {
+    temperature: 6,
+    weatherState: SUN,
+    humidity: 15,
+    wind: '10 m/s',
+}
 
 class WeatherLocation extends Component{
     render(){
         return(
             <div className="WeatherLocationCont">
                 <Location city={'San Pedro Sula'}/> 
-                <WeatherData/>
+                <WeatherData data={data}/>
             </div>
         )
     }
